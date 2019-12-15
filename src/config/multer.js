c2605bc141388eaad05147fb68c4d48b91f514da
jayce ayres <jayceayres@jayces-MacBook-Pro.local>
2019-12-15 17:50:49 -0500

    ajusting the size of file for multer

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,63 +1,57 @@
-const multer = require('multer')
-const path = require('path')
-const crypto = require('crypto')
-const aws = require('aws-sdk')
-const multerS3 = require('multer-s3')
+const multer = require("multer");
+const path = require("path");
+const crypto = require("crypto");
+const aws = require("aws-sdk");
+const multerS3 = require("multer-s3");
 
 const storageTypes = {
-  local:multer.diskStorage({
+  local: multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, path.resolve(__dirname, '..', '..', 'tmp', 'uploads'))
+      cb(null, path.resolve(__dirname, "..", "..", "tmp", "uploads"));
     },
     filename: (req, file, cb) => {
       crypto.randomBytes(16, (error, hash) => {
         if (error) cb(error);
 
-        file.key = `${hash.toString('hex')}-${file.originalname}`
+        file.key = `${hash.toString("hex")}-${file.originalname}`;
 
-        cb(null, file.key)
-      })
-    },
+        cb(null, file.key);
+      });
+    }
   }),
   s3: multerS3({
     s3: new aws.S3(),
-    bucket: 'effentiimage',
+    bucket: "effentiimage",
+    limits: { fieldSize: 25 * 1024 * 1024 },
     contentType: multerS3.AUTO_CONTENT_TYPE,
-    acl: 'public-read',
+    acl: "public-read",
     key: (req, file, cb) => {
       crypto.randomBytes(16, (error, hash) => {
         if (error) cb(error);
 
-        const fileName = `${hash.toString('hex')}-${file.originalname}`
+        const fileName = `${hash.toString("hex")}-${file.originalname}`;
 
-        cb(null, fileName)
-      })
-
-    },
+        cb(null, fileName);
+      });
+    }
   })
-}
-
+};
 
 module.exports = {
-
-  dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+  dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
   storage: storageTypes.s3,
-  limits: {
-
-  },
+  limits: {},
   fileFilter: (req, file, cb) => {
     const allowedMimes = [
-      'image/jpeg',
-      'image/pjpeg',
-      'image/png',
-      'image/gif'
+      "image/jpeg",
+      "image/pjpeg",
+      "image/png",
+      "image/gif"
     ];
     if (allowedMimes.includes(file.mimetype)) {
-      cb(null, true)
+      cb(null, true);
     } else {
       cb(new Error("Invalid file type"));
     }
   }
-
-
 };
